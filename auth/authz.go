37c2dad9f130115317fcf4156423ed8c15387c6e
Nandor Kracser <bonifaido@gmail.com>
2018-04-18 10:17:46 +0200

    wip

--- auth/authz.go
+++ auth/authz.go
@@ -3,33 +3,30 @@ package auth
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/banzaicloud/pipeline/model"
 	"github.com/casbin/casbin"
-	gormadapter "github.com/casbin/gorm-adapter"
+	"github.com/casbin/gorm-adapter"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
+
+var enforcer *casbin.SyncedEnforcer
 
 // NewAuthorizer returns the MySQL based default authorizer
 func NewAuthorizer() gin.HandlerFunc {
 	dbName := viper.GetString("database.dbname")
 	a := gormadapter.NewAdapter("mysql", model.GetDataSource(dbName), true)
-	e := casbin.NewEnforcer("authz_model.conf", a)
-	addDefaultPolicies(e, "bonifaido", 1)
-	if err := e.SavePolicy(); err != nil {
-		panic(err)
-	}
-	if err := e.LoadPolicy(); err != nil {
-		panic(err)
-	}
-	return newAuthorizer(e)
+	enforcer = casbin.NewSyncedEnforcer("authz_model.conf", a)
+	enforcer.StartAutoLoadPolicy(10 * time.Second)
+	return newAuthorizer(enforcer)
 }
 
 // NewAuthorizer returns the authorizer, uses a Casbin enforcer as input
-func newAuthorizer(e *casbin.Enforcer) gin.HandlerFunc {
+func newAuthorizer(e *casbin.SyncedEnforcer) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		a := &BasicAuthorizer{enforcer: e}
+		a := &BearerAuthorizer{enforcer: e}
 
 		if !a.CheckPermission(c.Request) {
 			a.RequirePermission(c)
@@ -37,21 +34,21 @@ func newAuthorizer(e *casbin.Enforcer) gin.HandlerFunc {
 	}
 }
 
-// BasicAuthorizer stores the casbin handler
-type BasicAuthorizer struct {
-	enforcer *casbin.Enforcer
+// BearerAuthorizer stores the casbin handler
+type BearerAuthorizer struct {
+	enforcer *casbin.SyncedEnforcer
 }
 
 // GetUserName gets the user name from the request.
-// Currently, only HTTP basic authentication is supported
-func (a *BasicAuthorizer) GetUserName(r *http.Request) string {
+// Currently, only HTTP Bearer token authentication is supported
+func (a *BearerAuthorizer) GetUserName(r *http.Request) string {
 	user := GetCurrentUser(r)
 	return user.Login
 }
 
 // CheckPermission checks the user/method/path combination from the request.
 // Returns true (permission granted) or false (permission forbidden)
-func (a *BasicAuthorizer) CheckPermission(r *http.Request) bool {
+func (a *BearerAuthorizer) CheckPermission(r *http.Request) bool {
 	user := a.GetUserName(r)
 	method := r.Method
 	path := r.URL.Path
@@ -59,17 +56,19 @@ func (a *BasicAuthorizer) CheckPermission(r *http.Request) bool {
 }
 
 // RequirePermission returns the 403 Forbidden to the client
-func (a *BasicAuthorizer) RequirePermission(c *gin.Context) {
-	c.Writer.WriteHeader(403)
-	c.Writer.Write([]byte("403 Forbidden\n"))
-	c.Abort()
+func (a *BearerAuthorizer) RequirePermission(c *gin.Context) {
+	c.AbortWithStatus(http.StatusForbidden)
 }
 
-func addDefaultPolicies(e *casbin.Enforcer, username string, orgids ...int) {
-	e.AddPolicy("bonifaido", "/api/v1/orgs", "*")
-	e.AddPolicy("bonifaido", "/api/v1/token", "*") // DEPRECATED
-	e.AddPolicy("bonifaido", "/api/v1/tokens", "*")
+func addDefaultPolicies(username string, orgids ...uint) {
+	enforcer.AddPolicy(username, "/api/v1/orgs", "*")
+	enforcer.AddPolicy(username, "/api/v1/token", "*") // DEPRECATED
+	enforcer.AddPolicy(username, "/api/v1/tokens", "*")
 	for _, orgid := range orgids {
-		e.AddPolicy("bonifaido", fmt.Sprintf("/api/v1/orgs/%d/*", orgid), "*")
+		enforcer.AddPolicy(username, fmt.Sprintf("/api/v1/orgs/%d", orgid), "*")
+		enforcer.AddPolicy(username, fmt.Sprintf("/api/v1/orgs/%d/*", orgid), "*")
+	}
+	if err := enforcer.SavePolicy(); err != nil {
+		panic(err)
 	}
 }
